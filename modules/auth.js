b6124f0e468b931e193db932aee913829728f6b7
Fierra <fiewwa@demc.store>
2024-07-03 08:27:27 +0800

    Removed Auth

--- modules/auth.js
+++ modules/auth.js
@@ -3,54 +3,6 @@ const db = require(process.env.mainFile).db;
 const Users = require(process.env.schemas);
 const jwt = require("jsonwebtoken");
 
-async function getToken(id, auth, ip) {
-  if (mongoose.isValidObjectId(id)) {
-    return await Users.findById(id).then((user) => {
-      if (!user) {
-        return { code: 404, res: "User Not Found" };
-      }
-      if (user.httpauth == auth) {
-        let tkn = jwt.sign({ userId: user._id }, process.env.key);
-        db.run(
-          `INSERT INTO tokens(token, ip) VALUES(?, ?)`,
-          [tkn, ip],
-          (err) => {
-            if (err) {
-              console.error(err.message);
-              return { code: 500, res: err.message };
-            }
-          }
-        );
-        return { code: 200, res: tkn };
-      } else {
-        return { code: 401, res: "Unauthorized" };
-      }
-    });
-  } else {
-    return await Users.findOne({ username: id }).then((user) => {
-      if (!user) {
-        return { code: 404, res: "User Not Found" };
-      }
-      if (user.httpauth == auth) {
-        let tkn = jwt.sign({ userId: user._id }, process.env.key);
-        db.run(
-          `INSERT INTO tokens(token, ip) VALUES(?, ?)`,
-          [tkn, ip],
-          (err) => {
-            if (err) {
-              console.error(err.message);
-              return { code: 500, res: err.message };
-            }
-          }
-        );
-        return { code: 200, res: tkn };
-      } else {
-        return { code: 401, res: "Unauthorized" };
-      }
-    });
-  }
-}
-
 async function verifyToken(token, ip) {
   try {
     const decoded = jwt.verify(token, process.env.key);
